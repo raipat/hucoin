d42a43f2eca3637ee49078272a4db811aa021106
Raipat <pat@schlafhacking.de>
2018-06-04 16:04:40 +0200

    basisparameter

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #hucoinTEST3\r");
+            Send(hSocket, "WHO #hucoinTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #hucoin00-#hucoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #hucoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #hucoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
